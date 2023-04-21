c95758898e1edf8ebed3f8162296c3dffbcc28f8
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-21 21:14:12 +0800

    Ask for user input

--- OOP-Decryptor.py
+++ OOP-Decryptor.py
@@ -1,6 +1,13 @@
 # Decryption_Marquez, Francis Ivan B._BSCpE 1-5
 
 #user input
+user_input = input("Enter encrypted text: ")
+
 #decryption
+# if *, change to a
+# if &, change to e
+# if #, change to i
+# if +, change to o
+# if !, change to u
 #output
 #additional
