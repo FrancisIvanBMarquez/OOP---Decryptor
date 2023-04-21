45fe2999c2ec6bcd151d964326edb85aa38020f6
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-21 21:55:44 +0800

    if &, change to e working

--- OOP-Decryptor.py
+++ OOP-Decryptor.py
@@ -10,6 +10,9 @@ for i in range(len(user_input)):
     if user_input[i] == "*":
         output += "a"
     # if &, change to e
+    # if &, change to e
+    elif user_input[i] == "&":
+        output += "e"
     # if #, change to i
     # if +, change to o
     # if !, change to u
