dab22a76bc0af00c9343886301c2809eded32bb0
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-04-21 21:50:52 +0800

    if *, change to a working

--- OOP-Decryptor.py
+++ OOP-Decryptor.py
@@ -2,14 +2,17 @@
 
 #user input
 user_input = input("Enter encrypted text: ")
-output = "test"
+output = ""
 
 #decryption
-# if *, change to a
-# if &, change to e
-# if #, change to i
-# if +, change to o
-# if !, change to u
+for i in range(len(user_input)):
+    # if *, change to a
+    if user_input[i] == "*":
+        output += "a"
+    # if &, change to e
+    # if #, change to i
+    # if +, change to o
+    # if !, change to u
 
 #output
 print(output)
